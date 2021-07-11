f7baec84a23c52467b30688938e71d8fe9741e6a
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-07-11 19:35:57 +0900

    Bump denops_std@v1.0.0-beta.4

--- denops/ghosttext/vendor/https/deno.land/x/denops_std/function/mod.ts
+++ denops/ghosttext/vendor/https/deno.land/x/denops_std/function/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/denops_std@v1.0.0-beta.0/function/mod.ts";
+export * from "https://deno.land/x/denops_std@v1.0.0-beta.4/function/mod.ts";
--- denops/ghosttext/vendor/https/deno.land/x/denops_std/mod.ts
+++ denops/ghosttext/vendor/https/deno.land/x/denops_std/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/denops_std@v1.0.0-beta.0/mod.ts";
+export * from "https://deno.land/x/denops_std@v1.0.0-beta.4/mod.ts";
