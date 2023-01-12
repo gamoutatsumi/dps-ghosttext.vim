4e721d1120b90296f971a9f80bf7018409f1fdb0
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-01-12 09:28:59 +0900

    update dependencies

--- denops/ghosttext/deps.ts
+++ denops/ghosttext/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.8.2/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.8.2/variable/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v3.8.2/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.8.2/autocmd/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.8.2/function/mod.ts";
-export * as opts from "https://deno.land/x/denops_std@v3.8.2/option/mod.ts";
-export { assertNumber } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v4.0.0/autocmd/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
+export * as opts from "https://deno.land/x/denops_std@v4.0.0/option/mod.ts";
+export { assertNumber } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
