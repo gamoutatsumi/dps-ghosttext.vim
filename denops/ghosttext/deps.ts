72d570c71f6831320f35db958f9bca3a2b525e22
gamoutatsumi <tatsumi@gamou.dev>
2024-04-15 10:38:28 +0900

    use unknownutil from jsr

--- denops/ghosttext/deps.ts
+++ denops/ghosttext/deps.ts
@@ -4,4 +4,4 @@ export * as helper from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 export * as opts from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
-export { assert, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
+export { assert, is } from "jsr:@core/unknownutil@3.18.0";
