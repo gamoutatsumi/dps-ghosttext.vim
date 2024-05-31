fa900da435a335a4e2d37a3e560ece26c6d8aeae
gamoutatsumi <tatsumi@gamou.dev>
2024-05-31 10:41:17 +0900

    update dependencies

--- denops/ghosttext/deps.ts
+++ denops/ghosttext/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
-export * as opts from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
-export { assert, is } from "jsr:@core/unknownutil@3.18.0";
+export type { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.5.0/variable/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v6.5.0/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.5.0/autocmd/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.5.0/function/mod.ts";
+export * as opts from "https://deno.land/x/denops_std@v6.5.0/option/mod.ts";
+export { assert, is } from "jsr:@core/unknownutil@3.18.1";
