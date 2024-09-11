cf0bc11ae510785aabe832232eafdabd874b8a10
gamoutatsumi <tatsumi@gamou.dev>
2024-09-11 11:51:49 +0900

    update deps

--- denops/ghosttext/deps.ts
+++ denops/ghosttext/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "jsr:@denops/std@7.0.2";
-export * as vars from "jsr:@denops/std@7.0.2/variable";
-export * as helper from "jsr:@denops/std@7.0.2/helper";
-export * as autocmd from "jsr:@denops/std@7.0.2/autocmd";
-export * as fn from "jsr:@denops/std@7.0.2/function";
-export * as opts from "jsr:@denops/std@7.0.2/option";
-export { assert, is } from "jsr:@core/unknownutil@^4.0.0";
+export type { Denops } from "jsr:@denops/std@7.1.1";
+export * as vars from "jsr:@denops/std@7.1.1/variable";
+export * as helper from "jsr:@denops/std@7.1.1/helper";
+export * as autocmd from "jsr:@denops/std@7.1.1/autocmd";
+export * as fn from "jsr:@denops/std@7.1.1/function";
+export * as opts from "jsr:@denops/std@7.1.1/option";
+export { assert, is } from "jsr:@core/unknownutil@4.3.0";
